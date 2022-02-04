68fbb5adcfaf786a29d57ccc25c5e435d071fe39
3ptipo1 <95851642+3ptipo1@users.noreply.github.com>
2022-02-04 12:44:00 +0100

    Update NSI-projet-3.py

--- NSI-projet-3.py
+++ NSI-projet-3.py
@@ -4,7 +4,7 @@ import csv
 import math
 import random
 
-def validation_croisee():
+def validation_croisee(characters):
     pass
 
 def kNN(characters, character, k = 5):
@@ -48,6 +48,8 @@ def kNN(characters, character, k = 5):
     # qui a demandé gryffondor au choixpeau)
         elif count == m: 
             m,decision = random.choice(((m,decision),(count,house)))
+    traduction = {"Gryffindor":"Gryffondor","Ravenclaw":"Serdaigle","Hufflepuff":"Poufsouffle","Slytherin":"Serpentard"}
+    decision = traduction[decision]
     return decision
     
 
@@ -75,4 +77,47 @@ with open("Caracteristiques_des_persos.csv", mode = "r", encoding = "utf-8") as 
                 del characters[-1]["Id"]
 
 # début du programme                
-kNN(characters, {'Courage': 2, 'Ambition': 9, 'Intelligence':2, 'Good':9})
+#kNN(characters, {'Courage': 2, 'Ambition': 9, 'Intelligence':2, 'Good':9})
+def main():
+    '''
+    Procédure lançant le programme, l'IHM ...
+    '''
+    k = 5
+    if input("Voulez vous choisir une valeur de k ? (y/n):") == "y":
+        k = int(input("rentrer votre valeur de k (entière et positive) :"))
+    elif input("Voulez vous choisir la valeur de k à l'aide d'une validation croisée ? (y/n):") == "y":
+        #k = validation_croisee(characters)
+        pass
+
+    moral_keys = ['Courage', 'Ambition', 'Intelligence', 'Good']
+    if input("Voulez vous tester les cas d'exemples ? (y/n):") == "y":
+        profiles = [{'Courage': 9, 'Ambition': 2, 'Intelligence':8, 'Good':9},
+                    {'Courage': 6, 'Ambition': 7, 'Intelligence':9, 'Good':7},
+                    {'Courage': 3, 'Ambition': 8, 'Intelligence':6, 'Good':3},
+                    {'Courage': 2, 'Ambition': 3, 'Intelligence':7, 'Good':8},
+                    {'Courage': 3, 'Ambition': 4, 'Intelligence':8, 'Good':8}]
+        for profile in profiles:
+            print("")
+            print("Pour un élève ayant les caractéristiques suivantes :")
+            for moral_key,value in profile.items():
+                print(f"{moral_key} : {value}",end=" | ")
+            print("")
+            print(f"La maison que le choixpeau magique recommande pour ce profil est {kNN(characters,profile,k)} !")
+            print("")
+    else:
+        character = dict()
+        print("Entrez les valeurs pour :")
+        for moral_key in moral_keys:
+            character[moral_key] = ""
+            while character[moral_key] == "":
+                try :
+                    character[moral_key] = int(input(f"- {moral_key} : "))
+                except :
+                    print("Rentrez un nombre entier positif !")
+        print(f"La maison que le choixpeau magique recommande pour ce profil est {kNN(characters,character,k)} !")
+    if input("Voulez vous continuez ? (y/n):") == "y":
+        main()
+    else:
+        return None
+main()
+            
